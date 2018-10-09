20577b441a3db5c89232fcce15b84bc106c62d3e
jeasonyoung <jeason1914@qq.com>
2018-10-09 11:47:27 +0800

    添加门栓工具类消费时间处理

--- young-common/src/main/java/org/young/common/util/LatchUtil.java
+++ young-common/src/main/java/org/young/common/util/LatchUtil.java
@@ -44,6 +44,7 @@ public class LatchUtil {
      * 线程
      * @param command
      * 业务处理
+     * @return 工具对象
      */
     public LatchUtil addExecutor(@Nonnull final Executor executor, @Nonnull final Runnable command){
         log.debug("addExecutor(executor: {}, command: {})...", executor, command);
@@ -53,12 +54,15 @@ public class LatchUtil {
         }
         //异步线程处理
         executor.execute(()->{
+            final long start = System.currentTimeMillis();
             try{
                 command.run();
             }catch (Throwable ex){
                 log.warn("addExecutor-command-exp:" + ex.getMessage(), ex);
             }finally {
                 downLatch.countDown();
+                final long interval = System.currentTimeMillis() - start;
+                log.info("addExecutor-线程执行时间: {}ms", interval);
             }
         });
         return this;
@@ -68,10 +72,14 @@ public class LatchUtil {
      * 等待执行完成
      */
     public void await(){
+        final long start = System.currentTimeMillis();
         try {
             downLatch.await();
         }catch (Throwable ex){
             log.warn("await-exp:" + ex.getMessage(), ex);
+        }finally {
+            final long interval = System.currentTimeMillis() - start;
+            log.info("await-线程等待时间: {}ms", interval);
         }
     }
 }
