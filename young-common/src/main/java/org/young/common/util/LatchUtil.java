8534c1dcc157fc65add53402c3ba98c389b5642e
jeasonyoung <jeason1914@qq.com>
2018-10-09 09:44:27 +0800

    添加 Pageable 分页处理

---
+++ young-common/src/main/java/org/young/common/util/LatchUtil.java
@@ -0,0 +1,77 @@
+package org.young.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 门栓工具类
+ *
+ * @author yangyong young1982@foxmail.com
+ * date 2018/10/9 09:37
+ */
+@Slf4j
+public class LatchUtil {
+    private final CountDownLatch downLatch;
+    private final AtomicInteger count;
+    /**
+     * 构造函数
+     * @param count
+     * 门栓个数
+     */
+    private LatchUtil(final int count){
+        this.downLatch = new CountDownLatch(count);
+        this.count = new AtomicInteger(count);
+    }
+
+    /**
+     * 创建门栓处理实例
+     * @param count
+     * 门栓个数
+     * @return 处理实例
+     */
+    public static LatchUtil create(int count){
+        log.debug("create(count: {})...", count);
+        return new LatchUtil(count);
+    }
+
+    /**
+     * 添加线程执行处理
+     * @param executor
+     * 线程
+     * @param command
+     * 业务处理
+     */
+    public LatchUtil addExecutor(@Nonnull final Executor executor, @Nonnull final Runnable command){
+        log.debug("addExecutor(executor: {}, command: {})...", executor, command);
+        final int current = this.count.decrementAndGet();
+        if(current <= -1){
+            throw new RuntimeException("超出门栓个数!");
+        }
+        //异步线程处理
+        executor.execute(()->{
+            try{
+                command.run();
+            }catch (Throwable ex){
+                log.warn("addExecutor-command-exp:" + ex.getMessage(), ex);
+            }finally {
+                downLatch.countDown();
+            }
+        });
+        return this;
+    }
+
+    /**
+     * 等待执行完成
+     */
+    public void await(){
+        try {
+            downLatch.await();
+        }catch (Throwable ex){
+            log.warn("await-exp:" + ex.getMessage(), ex);
+        }
+    }
+}
