ec47219e8eb355e8ab78ce15c046f2205e2b19a2
jeasonyoung <jeason1914@qq.com>
2018-10-15 16:03:51 +0800

    添加令牌用户数据静态构造方法

--- young-common/src/main/java/org/young/common/protocol/provider/TokenUser.java
+++ young-common/src/main/java/org/young/common/protocol/provider/TokenUser.java
@@ -2,10 +2,11 @@ package org.young.common.protocol.provider;
 
 import lombok.Data;
 
+import javax.annotation.Nonnull;
 import java.io.Serializable;
 
 /**
- * 令牌用户-数据接口.
+ * 令牌用户数据
  * @author jeasonyoung
  */
 @Data
@@ -18,4 +19,21 @@ public class TokenUser implements Serializable {
      * 登录ID
      */
     private String loginId;
+
+    /**
+     * 创建令牌用户数据
+     * @param loginId
+     * 登录ID
+     * @param userId
+     * 用户ID
+     * @return 令牌用户数据
+     */
+    public static TokenUser create(@Nonnull final String loginId,@Nonnull final String userId){
+        final TokenUser data = new TokenUser();
+        //设置登录ID
+        data.setLoginId(loginId);
+        //设置用户ID
+        data.setUserId(userId);
+        return data;
+    }
 }
