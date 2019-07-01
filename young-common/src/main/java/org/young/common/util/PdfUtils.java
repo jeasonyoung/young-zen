29eb5873bd2d0beacd7ad7261c31e252e0a8498a
杨勇 <yangyong@wosaiapp.com>
2019-07-01 17:04:16 +0800

    升级版本到1.0.10

---
+++ young-common/src/main/java/org/young/common/util/PdfUtils.java
@@ -0,0 +1,91 @@
+package org.young.common.util;
+
+import com.google.common.base.Strings;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * PDF-工具类
+ *
+ * @author yangyong
+ * @version 1.0
+ **/
+@Slf4j
+public class PdfUtils {
+    /**
+     * 线程池
+     */
+    private static final ExecutorService POOLS = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(), new ThreadFactoryBuilder().setNameFormat("pools-pdf-%d").build());
+
+    /**
+     * 生成PDF文件流
+     * @param url
+     * url
+     * @param output
+     * PDF输出流
+     * @throws Exception
+     * 异常
+     */
+    public static synchronized void createPdf(@Nonnull final String url,@Nonnull final OutputStream output) throws Exception {
+        log.debug("createPdf(url: {})...", url);
+        //检查参数
+        Assert.hasText(url, "'url'不能为空!");
+        //准备执行外部进程
+        final ProcessBuilder processBuilder = new ProcessBuilder(Arrays.asList("wkhtmltopdf", url, "-"));
+        //执行PDF生成进程
+        final Process pdfProcess = processBuilder.start();
+        //异步线程处理
+        POOLS.execute(() -> {
+            //清理错误流
+            clearProcessErrorStream(pdfProcess.getErrorStream());
+        });
+        //数据流读取
+        try (InputStream in = pdfProcess.getInputStream()) {
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf, 0, buf.length)) != -1) {
+                output.write(buf, 0, len);
+            }
+        } catch (Throwable ex) {
+            log.error("createPdf(url: " + url + ")-读取PDF流异常-exp:" + ex.getMessage(), ex);
+            throw new Exception(ex.getMessage());
+        } finally {
+            //销毁外部进程
+            pdfProcess.destroy();
+        }
+    }
+
+    /**
+     * 清除进程错误流数据
+     * @param in
+     * 输入流
+     */
+    private static void clearProcessErrorStream(final InputStream in){
+        if(in == null){
+            return;
+        }
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))){
+            String context;
+            while ((context = reader.readLine()) != null){
+                if(!Strings.isNullOrEmpty(context)) {
+                    log.debug("clearProcessErrorStream: {}", context);
+                }
+            }
+        }catch (Throwable ex){
+            log.warn("clearProcessErrorStream-exp: {}", ex.getMessage());
+        }
+    }
+}
